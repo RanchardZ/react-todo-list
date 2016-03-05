244503a59131ab137fd7cdb083c2ae890a31159b
RanchardZ <ranchardzheng@ruc.edu.cn>
2016-03-05 15:27:48 +0800

    gone through the part3

--- client/client.js
+++ client/client.js
@@ -1,1 +1,15 @@
 
+import React from 'react'  
+import { render } from 'react-dom' 
+import App from '../components/App'
+
+// var React = require('react')
+// var render = require('react-dom').render;
+render(
+	// define the encompassing component,
+	// DOM element we want to mount it to
+
+	// JSX syntax
+	<App/>,
+	document.getElementById('app')
+)
--- webpack.config.js
+++ webpack.config.js
@@ -1,1 +1,22 @@
 
+module.exports = {
+	devtool: 'inline-source-map', // not in production code -- this is used to track the error in source files instead of bundle.js
+	entry: ['./client/client.js'],
+	output: {
+		path: './dist',
+		filename: 'bundle.js',
+		publicPath: '/'
+	},
+	module: {
+		loaders: [
+			{
+				test: /\.js$/,
+				loader: 'babel-loader', // compile JSX and ES6 into JavaScript
+				exclude: /node_modules/,
+				query: {
+					presets: ['react', 'es2015']
+				} // equivalent to setting a babel setting file
+			}
+		]
+	} // certain tasks you want webpack do
+}
